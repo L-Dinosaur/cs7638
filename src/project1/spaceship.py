7d745600727157cde2d1dde0f14e31f52edf0908
L-Dinosaur <hao.dino.liu@gmail.com>
2026-09-19 16:59:59 -0400

    initial implementation of jump

--- src/project1/spaceship.py
+++ src/project1/spaceship.py
@@ -22,7 +22,7 @@
 #   Gradescope), in which case you should download a fresh copy of all the
 #   project files.
 from rait import matrix
-
+import math
 # constant F, H, and R for the six variable state system
 F = matrix([[1, 0, 1, 0, 0.5, 0],
             [0, 1, 0, 1, 0, 0.5],
@@ -40,11 +40,46 @@ R = matrix([[0.045 * 0.45, 0],
 P_init = matrix()
 P_init.identity(3)
 
+COLD_START = 10
+X_BUFFER = 0.1
+Y_BUFFER = 0.1
+JUMP_BUFFER = 0.2
+
 OUTPUT_UNIQUE_FILE_ID = False
 if OUTPUT_UNIQUE_FILE_ID:
     import hashlib, pathlib
     file_hash = hashlib.md5(pathlib.Path(__file__).read_bytes()).hexdigest()
     print(f'Unique file ID: {file_hash}')
+
+
+def euc_distance(point_1, point_2):
+    return math.sqrt((point_1[0] - point_2[0])**2 + (point_1[1] - point_2[1])**2)
+
+def rank_dict_vals(d):
+    ranked_v = []
+    ranked_k = []
+    res = {}
+    for k, v in d.items():
+        if not ranked_v:
+            ranked_v.append(v)
+            ranked_k.append(k)
+            continue
+        idx = 0
+        while idx < len(ranked_v):
+            if v < ranked_v[idx]:
+                idx += 1
+            else:
+                break
+        ranked_v.insert(idx, v)
+        ranked_k.insert(idx, k)
+    for i, k in enumerate(ranked_k):
+        res[k] = i
+    return res
+
+def elem_addition(a, b):
+    if len(a) != len(b):
+        raise ValueError('A and B must have same length')
+    return [ax + bx for ax, bx in zip(a, b)]
 
 class Spaceship():
     """A class representing the Environment within which the spaceship will run,
@@ -57,6 +92,10 @@ class Spaceship():
         self.agent_pos_start = xy_start
         self.asteroid_states = None
         self.asteroid_uncertainty = None
+        self.cold_start = COLD_START
+        self.x_buffer = self.x_bounds[1] * X_BUFFER
+        self.y_buffer = self.y_bounds[1] * Y_BUFFER
+        self.asteroid_ridden = None
 
     def _init_single_asteroid(self, coordinate):
         state = matrix([[coordinate[0], coordinate[1], 0, 0, 0, 0]]).transpose()
@@ -106,7 +145,7 @@ class Spaceship():
         # To view the visualization with the default pdf output (incorrect) uncomment the line below
         # return asteroid_observations
 
-        # FOR STUDENT TODO: Update the Spaceship's estimate of where the asteroids will be located in the next time step
+        # FOR STUDENT
         if self.asteroid_states is None:
             self._init_asteroids(asteroid_observations)
 
@@ -138,6 +177,76 @@ class Spaceship():
 
         return {i: (self.asteroid_states[i][0][0], self.asteroid_states[i][1][0]) for i in asteroid_observations.keys()}
 
+    def find_jumpable_asteroids(self, agent_data):
+        res = {}
+        if agent_data['ridden_asteroid'] is None:
+            starting_pos = self.agent_pos_start
+        else:
+            starting_pos = (self.asteroid_states[agent_data['ridden_asteroid']][0][0], self.asteroid_states[agent_data['ridden_asteroid']][1][0])
+        for i, state in self.asteroid_states.items():
+            if i == agent_data['ridden_asteroid']:
+                continue
+            coordinate = (state[0][0], state[1][0])
+            if euc_distance(starting_pos, coordinate) < agent_data['jump_distance'] * (1 - JUMP_BUFFER):
+                res[i] = coordinate
+        return res
+
+    def _near_y_bound(self, location):
+        return location[1] < self.y_buffer
+
+    def _near_x_bound_left(self, location):
+        return location[0] < self.x_buffer
+
+    def _near_x_bound_right(self, location):
+        return (self.x_bounds[1] - location[0]) < self.x_buffer
+
+
+    def _jump_back_x(self, available_asteroid_pos):
+        dist_tracker = -1 * math.inf
+        target = None
+        for i, coordinate in available_asteroid_pos.items():
+            dist = math.sqrt(coordinate[0]**2 + (coordinate[0] - self.x_bounds[1])**2)
+            if math.sqrt(coordinate[0]**2 + (coordinate[0] - self.x_bounds[1])**2) > dist_tracker:
+                target = i
+                dist_tracker = dist
+        return target
+
+    @staticmethod
+    def _jump_back_y(available_asteroid_pos):
+        dist_tracker = -1 * math.inf
+        target = None
+        for i, coordinate in available_asteroid_pos.items():
+            if coordinate[1] > dist_tracker:
+                target = i
+                dist_tracker = coordinate[1]
+        return target
+
+    def _find_better_asteroid(self, available_asteroid_pos):
+        available_asteroids = available_asteroid_pos.keys()
+        positions = {}
+        velocities = {}
+        accelerations = {}
+        for i in available_asteroids:
+            state = self.asteroid_states[i]
+            positions[i] = state[1][0]
+            velocities[i] = state[3][0]
+            accelerations[i] = state[5][0]
+
+        ranking_pos = rank_dict_vals(positions)
+        ranking_vel = rank_dict_vals(velocities)
+        ranking_acc = rank_dict_vals(accelerations)
+
+        ranking = {k: v for k, v in zip(ranking_pos.keys(), elem_addition(elem_addition(ranking_pos.values(), ranking_vel.values()), ranking_acc.values()))}
+
+        ranking_holder = math.inf
+        target = None
+        for k, v in ranking.items():
+            if v < ranking_holder:
+                target = k
+                ranking_holder = v
+
+        return target
+
     def jump(self, asteroid_observations, agent_data):
         """ Return the id of the asteroid the spaceship should jump/hop onto in the next timestep
         ----------
@@ -168,12 +277,55 @@ class Spaceship():
         return 3, estimated_return
 
         """
-        # FOR STUDENT TODO: Update the idx of the asteroid on which to jump
-        idx = False
+        # FOR STUDENT
+        # Predict
+        asteroid_pos_predictions = self.predict_from_observations(asteroid_observations)
 
-        return idx, None
+        # Wait till KF calibrate
+        if self.cold_start > 0:
+            self.cold_start -= 1
+            return None, None
+
+        # Find available asteroids to jump to
+        available_asteroids = self.find_jumpable_asteroids(agent_data)
+        if not available_asteroids:
+            return None, None
+
+        if agent_data['ridden_asteroid'] is None:
+            target = self._find_better_asteroid(available_asteroids)
+            return target, asteroid_pos_predictions[target]
+        else:
+            current_state = self.asteroid_states[agent_data['ridden_asteroid']]
+            current_pos = (current_state[0][0], current_state[1][0])
+            # 1. if near bottom boundary
+            if self._near_y_bound(current_pos) and self.asteroid_states[agent_data['ridden_asteroid']][3][0] < 0:
+                target = self._jump_back_y(available_asteroids)
+                return target, asteroid_pos_predictions[target]
+
+            # 2. if near side boundaries
+            if (self._near_x_bound_left(current_pos) and (self.asteroid_states[agent_data['ridden_asteroid']][2][0] < 0))\
+                    or (self._near_x_bound_right(current_pos) and (self.asteroid_states[agent_data['ridden_asteroid']][2][0] > 0)):
+                target = self._jump_back_x(available_asteroids)
+                return target, asteroid_pos_predictions[target]
+
+            # 3. if not near boundaries
+            available_asteroids.update({agent_data['ridden_asteroid']: asteroid_pos_predictions[agent_data['ridden_asteroid']]})
+            target = self._find_better_asteroid(available_asteroids)
+            if target == agent_data['ridden_asteroid']:
+                return None, None
+            else:
+                return target, asteroid_pos_predictions[target]
+
 
 def who_am_i():
     # Please specify your GT login ID in the whoami variable (ex: jsmith326).
     whoami = 'test'
     return whoami
+
+if __name__ == '__main__':
+
+    # Test my utility functions
+
+    # Test rank_dict
+    dict_1 = {'a': 3, 'b': 5, 'e': 27, 'z': 9, 'xx': 7}
+    print(rank_dict_vals(dict_1))
